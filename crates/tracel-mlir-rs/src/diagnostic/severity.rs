d61c66420e9a357d77381cb55cf48ca8dcb0cbdc
Sylvain Benner <sylvain@benner.online>
2025-09-15 16:57:51 -0400

    fix: support i32 diagnostic severity constants on Windows

--- crates/tracel-mlir-rs/src/diagnostic/severity.rs
+++ crates/tracel-mlir-rs/src/diagnostic/severity.rs
@@ -13,17 +13,26 @@ pub enum DiagnosticSeverity {
     Warning,
 }
 
+impl TryFrom<i32> for DiagnosticSeverity {
+    type Error = Error;
+
+    fn try_from(severity: i32) -> Result<Self, Error> {
+        #[allow(non_upper_case_globals)]
+        Ok(match severity {
+            MlirDiagnosticSeverity_MlirDiagnosticError  => Self::Error,
+            MlirDiagnosticSeverity_MlirDiagnosticNote   => Self::Note,
+            MlirDiagnosticSeverity_MlirDiagnosticRemark => Self::Remark,
+            MlirDiagnosticSeverity_MlirDiagnosticWarning=> Self::Warning,
+            _ => return Err(Error::UnknownDiagnosticSeverity(severity as u32)),
+        })
+    }
+}
+
 impl TryFrom<u32> for DiagnosticSeverity {
     type Error = Error;
 
     fn try_from(severity: u32) -> Result<Self, Error> {
-        #[allow(non_upper_case_globals)]
-        Ok(match severity {
-            MlirDiagnosticSeverity_MlirDiagnosticError => Self::Error,
-            MlirDiagnosticSeverity_MlirDiagnosticNote => Self::Note,
-            MlirDiagnosticSeverity_MlirDiagnosticRemark => Self::Remark,
-            MlirDiagnosticSeverity_MlirDiagnosticWarning => Self::Warning,
-            _ => return Err(Error::UnknownDiagnosticSeverity(severity)),
-        })
+        // bindgen may use i32 on some targets like windows
+        (severity as i32).try_into()
     }
 }
