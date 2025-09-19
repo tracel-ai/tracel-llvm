73c01ec1b039d50d79a3f63c5f3fea60561ab1e4
Sylvain Benner <sylvain.benner@gmail.com>
2025-09-19 03:56:16 -0400

    fix: add Publish command

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,6 @@
 use tracel_xtask::prelude::*;
 
-#[macros::base_commands(Build, Bump, Check, Compile, Fix, Test)]
+#[macros::base_commands(Build, Bump, Check, Compile, Fix, Test, Publish)]
 enum Command {}
 
 fn main() -> anyhow::Result<()> {
