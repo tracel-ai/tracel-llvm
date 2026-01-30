b78a1716453b36940f9a6b1702792c224ee3236c
Simeon H.K. Fitch <simeon.fitch@datashapesai.com>
2026-01-30 10:54:28 -0500

    Initial bindings for linux-aarch64

--- crates/tracel-mlir-sys/src/bindings/mod.rs
+++ crates/tracel-mlir-sys/src/bindings/mod.rs
@@ -6,6 +6,18 @@
 
 #[cfg(all(
     not(feature = "xtask"),
+    all(target_os = "linux", target_arch = "aarch64")
+))]
+mod bindings_linux_aarch64;
+
+#[cfg(all(
+    not(feature = "xtask"),
+    all(target_os = "linux", target_arch = "aarch64")
+))]
+pub use bindings_linux_aarch64::*;
+
+#[cfg(all(
+    not(feature = "xtask"),
     all(target_os = "linux", target_arch = "x86_64")
 ))]
 mod bindings_linux_x86_64;
@@ -43,6 +55,7 @@ pub use bindings_windows_x86_64::*;
 #[cfg(all(
     not(feature = "xtask"),
     not(any(
+        all(target_os = "linux", target_arch = "aarch64"),
         all(target_os = "linux", target_arch = "x86_64"),
         all(target_os = "macos", target_arch = "aarch64"),
         all(target_os = "windows", target_arch = "x86_64"),
