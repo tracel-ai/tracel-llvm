c5e23b087da39be67c4764b93005f12b87ff0bd0
syl20bnr <sylvain.benner@tracel.ai>
2026-05-01 15:51:14 -0400

    chore: update macos bindings

--- crates/tracel-tblgen-rs/src/bindings/bindings_macos_aarch64.rs
+++ crates/tracel-tblgen-rs/src/bindings/bindings_macos_aarch64.rs
@@ -747,7 +747,7 @@ unsafe extern "C" {
 }
 unsafe extern "C" {
     pub fn tableGenRecordValGetValAsBit(rv_ref: TableGenRecordValRef, bit: *mut i8)
-    -> TableGenBool;
+        -> TableGenBool;
 }
 unsafe extern "C" {
     pub fn tableGenRecordValGetValAsBits(rv_ref: TableGenRecordValRef, len: *mut usize) -> *mut i8;
@@ -781,7 +781,7 @@ unsafe extern "C" {
 }
 unsafe extern "C" {
     pub fn tableGenDagRecordGet(rv_ref: TableGenTypedInitRef, index: usize)
-    -> TableGenTypedInitRef;
+        -> TableGenTypedInitRef;
 }
 unsafe extern "C" {
     pub fn tableGenDagRecordArgName(
